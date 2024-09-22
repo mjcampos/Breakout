b2ec8b3fd6a2d92bdf169f1a3f6c30d7c822b22a
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-22 17:58:04 +0200

    Update player.gd
    Added ability to quit game by pressing escape key

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @export var _speed: float = 400.0
 @onready var sprite_2d = $Sprite2D
 
+func _process(delta):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
 func _physics_process(_delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
