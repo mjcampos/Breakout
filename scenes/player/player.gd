f2650d388c8a2b46089266b31d1a882c06436420
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-23 11:48:05 +0200

    New setup of ball
    Last setup didn't feel right, codewise. So made a quick redesign

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+class_name Player
 @export var _speed: float = 400.0
 @onready var sprite_2d = $Sprite2D
 
@@ -7,10 +7,6 @@ var fixed_y_position: float
 
 func _ready():
 	fixed_y_position = position.y
-
-func _process(delta):
-	if Input.is_key_pressed(KEY_ESCAPE):
-		get_tree().quit()
 
 func _physics_process(_delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
