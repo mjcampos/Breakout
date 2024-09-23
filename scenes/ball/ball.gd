cb1ee111976e390d0c97d560dc968b2749cfab65
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-23 19:18:37 +0200

    Added explosion and hit audio

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -1,6 +1,9 @@
 class_name Ball
 extends CharacterBody2D
 
+@onready var explosion_audio = $ExplosionAudio
+@onready var hit_audio = $HitAudio
+
 var speed = 500.0
 
 # Called when the node enters the scene tree for the first time.
@@ -19,4 +22,9 @@ func _physics_process(delta):
 		var collider: Object = collision.get_collider()
 		
 		if collider is Brick:
+			explosion_audio.position = position
+			explosion_audio.play()
 			collider.destroy_brick()
+		else:
+			hit_audio.position = position
+			hit_audio.play()
