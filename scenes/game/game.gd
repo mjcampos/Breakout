690c50931de507277c165ebf80c79c2fedc5f9c3
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-23 19:46:28 +0200

    Added Dead zone
    When player fails to catch ball it goes into the dead zone and they loose a life.

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,10 +1,11 @@
+class_name Game
 extends Node2D
 
 @export var _points: int = 0
 @export var _lives: int = 3
 
-@onready var point_label = $"Point Label"
-@onready var lives_label = $"Lives Label"
+@onready var point_label = $"labels/Point Label"
+@onready var lives_label = $"labels/Lives Label"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
