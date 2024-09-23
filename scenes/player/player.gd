238102b510661163d10ef29106b69782a643ef92
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-23 10:09:05 +0200

    Update player.gd
    Set fixed Y position to prevent ball from pushing paddle downwards each time they collide

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 @export var _speed: float = 400.0
 @onready var sprite_2d = $Sprite2D
 
+var fixed_y_position: float
+
+func _ready():
+	fixed_y_position = position.y
+
 func _process(delta):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
@@ -11,5 +16,6 @@ func _physics_process(_delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	velocity.x = direction * _speed
+	position.y = fixed_y_position
 	
 	move_and_slide()
