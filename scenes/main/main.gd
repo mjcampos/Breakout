6bef0dc0f6d8f2f20521efb3d4fe92a274fdcf14
Michael Campos <mjcampos@users.noreply.github.com>
2024-10-30 01:55:57 +0100

    Can begin play with xbox controller
    Player can now focus and trigger play button with xbox controller.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,4 +1,10 @@
 extends Control
 
+@onready var play_button: Button = $PlayButton
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+		play_button.grab_focus()
+
 func _on_play_button_pressed():
 	GameManager.go_to_game()
